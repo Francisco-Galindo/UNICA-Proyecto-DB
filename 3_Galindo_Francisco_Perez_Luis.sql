be86a04b3d4bc5fb6a4dc4d3e8e2971eb087f78f
Francisco-Galindo <paqui10718@gmail.com>
2023-04-02 00:47:13 -0600

    Creación de la base de datos y llenado con script

---
+++ 3_Galindo_Francisco_Perez_Luis.sql
@@ -0,0 +1,159 @@
+\c postgres postgres
+drop database distromachines;
+create database distromachines encoding 'utf-8';
+\c distromachines
+
+create sequence empresa_seq increment by 1 minvalue 0 no maxvalue;
+create sequence cuenta_seq increment by 1 minvalue 0 no maxvalue;
+create sequence dulce_seq increment by 1 minvalue 0 no maxvalue;
+create sequence contrato_seq increment by 1 minvalue 0 no maxvalue;
+create sequence maquina_seq increment by 1 minvalue 0 no maxvalue;
+create sequence contrato_1b_dulce_seq increment by 1 minvalue 0 no maxvalue;
+
+create table empresa (
+	empresa_id numeric(10, 0) default nextval('empresa_seq'),
+	nombre varchar(100) not null,
+	representante varchar(100) not null,
+	constraint empresa_pk
+		primary key (empresa_id)
+);
+
+
+create table dulce (
+	dulce_id numeric(10, 0) default nextval('dulce_seq'),
+	nombre varchar(60) not null,
+	tipo varchar(20) not null,
+	marca varchar(60) not null,
+	precio_adquisicion numeric(7, 2) not null,
+	constraint dulce_pk
+		primary key (dulce_id)
+);
+
+create table cuenta_bancaria (
+	cuenta_bancaria_id numeric(10, 0) default nextval('cuenta_seq'),
+	empresa_id numeric(10, 0) not null,
+	clabe char(18) not null,
+	banco varchar(40) not null,
+	administrador varchar(100) not null,
+	constraint empresa_cuenta_fk
+		foreign key (empresa_id)
+		references empresa(empresa_id)
+		on delete restrict on update cascade,
+	constraint cuenta_pk
+		primary key (cuenta_bancaria_id, empresa_id)
+);
+
+create table contrato (
+	contrato_id numeric(10, 0) default nextval('contrato_seq'),
+	vigencia date not null,
+	clave varchar(10) not null,
+	empresa_id numeric(10, 0) not null,
+	constraint empresa_contrato_fk
+		foreign key (empresa_id)
+		references empresa(empresa_id)
+		on delete restrict on update cascade,
+	constraint contrato_pk
+		primary key (contrato_id)
+);
+
+create table contrato_1a (
+	contrato_id numeric(10, 0) not null,
+	precio_maquina numeric(10,2) not null,
+	constraint contrato_contrato_1a_fk
+		foreign key (contrato_id)
+		references contrato(contrato_id)
+		on delete restrict on update cascade,
+	constraint contrato_1a_pk
+		primary key (contrato_id)
+);
+
+create table contrato_1b (
+	contrato_id numeric(10, 0) not null,
+	precio_maquina numeric(10,2) not null,
+	fecha_resurtir date not null,
+	constraint contrato_contrato_1b_fk
+		foreign key (contrato_id)
+		references contrato(contrato_id)
+		on delete restrict on update cascade,
+	constraint contrato_1b_pk
+		primary key (contrato_id)
+);
+
+create table maquina (
+	maquina_id numeric(10, 0) default nextval('maquina_seq'),
+	material varchar(40) not null,
+	tipo_sistema_operativo varchar(40) not null,
+	num_registro varchar(10) not null,
+	empresa_id numeric(10, 0) not null,
+	contrato_id numeric(10, 0) not null,
+	constraint empresa_maquina_fk
+		foreign key (empresa_id)
+		references empresa(empresa_id)
+		on delete restrict on update cascade,
+	constraint contrato_maquina_fk
+		foreign key (contrato_id)
+		references contrato(contrato_id)
+		on delete restrict on update cascade,
+	constraint maquina_pk
+		primary key (maquina_id)
+);
+
+create table tipo_A (
+	maquina_id numeric(10, 0) not null,
+	dimensiones varchar(12),
+	capacidad numeric(2, 0) not null,
+	constraint maquina_tipo_A_fk
+		foreign key (maquina_id)
+		references maquina(maquina_id)
+		on delete restrict on update cascade,
+	constraint tipo_A_pk
+		primary key (maquina_id)
+);
+
+create table tipo_B (
+	maquina_id numeric(10, 0) not null,
+	dimensiones varchar(12),
+	capacidad numeric(2, 0) not null,
+	vidrio_reforzado varchar(20) not null,
+	alerta varchar(20) null,
+	candados varchar(20) null,
+	constraint maquina_tipo_C_fk
+		foreign key (maquina_id)
+		references maquina(maquina_id)
+		on delete restrict on update cascade,
+	constraint tipo_C_pk
+		primary key (maquina_id)
+);
+
+create table tipo_C (
+	maquina_id numeric(10, 0) not null,
+	dimensiones varchar(12),
+	capacidad numeric(2, 0) not null,
+	vidrio_templado varchar(20) not null,
+	alerta varchar(20) not null,
+	candados varchar(20) not null,
+	camara varchar(20) not null,
+	constraint tipo_C_fk
+		foreign key (maquina_id)
+		references maquina(maquina_id)
+		on delete restrict on update cascade,
+	constraint maquina_tipo_C_pk
+		primary key (maquina_id)
+);
+
+create table contrato_1b_dulce (
+	contrato_1b_dulce_id numeric(10, 0) default nextval('contrato_1b_dulce_seq'),
+	precio_dulces numeric(10, 2) not null,
+	dulce_id numeric(10, 0) not null,
+	contrato_id numeric(10, 0) not null,
+	constraint contrato_1b_contrato_1b_dulce_fk
+		foreign key (contrato_id)
+		references contrato(contrato_id)
+		on delete restrict on update cascade,
+	constraint dulce_contrato_1b_dulce_fk
+		foreign key (dulce_id)
+		references dulce(dulce_id)
+		on delete restrict on update cascade,
+	constraint contrato_1b_dulce_pk
+		primary key (contrato_1b_dulce_id)
+);
---
+++ 4_Galindo_Francisco_Perez_Luis.sql
@@ -0,0 +1,187 @@
+with datos_dulce as (
+	select
+	'dulce_' || seq as nombre,
+	(
+		case (random() * 3)::int
+			when 0 then 'dulce'
+			when 1 then 'salado'
+			when 2 then 'picoso'
+			when 3 then 'acidulzado'
+		end
+	) as tipo,
+	(
+		case (random() * 3)::int
+			when 0 then 'marinela'
+			when 1 then 'sabritas'
+			when 2 then 'nestle'
+			when 3 then 'loquesea'
+		end
+	) as marca,
+	(random() * 10000) as precio_adquisicion
+	from GENERATE_SERIES(1, 100) seq
+)
+insert into dulce(nombre, tipo, marca, precio_adquisicion) select * from datos_dulce;
+select * from dulce;
+
+with datos_empresa as (
+	select
+	'empresa_' || seq AS nombre,
+	'representante_' || seq AS representante
+	from GENERATE_SERIES(1, 100) seq
+)
+insert into empresa(nombre, representante) select * from datos_empresa;
+select * from empresa;
+
+with datos_cuenta as (
+	select
+	empresa_id,
+	CAST(100000000000000000 + floor(random() * 900000000000000000) as bigint) as clabe,
+	(
+		case (random() * 2)::int
+			when 0 then 'Banamex'
+			when 1 then 'Bancomer'
+			when 2 then 'BBVA'
+		end
+	) as banco,
+	'administrador_' || seq as administrador
+	from GENERATE_SERIES(0, (select count(empresa_id) from empresa)) seq
+	inner join empresa ON (seq = empresa_id)
+)
+insert into cuenta_bancaria(empresa_id, clabe, banco, administrador) select * from datos_cuenta;
+select * from cuenta_bancaria;
+
+with expanded as (
+	select random(), seq, empresa_id
+	FROM GENERATE_SERIES(1, 100) seq, empresa
+), shuffled as (
+	select
+	date(current_date + (random() * 365 * 5)::int) as vigencia,
+	'BORRAME' as clabe,
+	e.empresa_id
+	from expanded e
+	inner join (
+		select ei.seq, MIN(ei.random) from expanded ei group by ei.seq
+	) em ON (e.seq = em.seq AND e.random = em.min)
+)
+insert into contrato(vigencia, clave, empresa_id) select * from shuffled;
+select * from contrato;
+
+do $$
+declare
+	counter integer := 0;
+begin
+	while counter < (select count(contrato_id) from contrato) loop
+		if (random())::int = 0 then
+			insert into contrato_1a(contrato_id, precio_maquina) values(counter, random() * 10000);
+		else
+			insert into contrato_1b(contrato_id, fecha_resurtir, precio_maquina) values(counter, date(current_date + 30), random() * 10000);
+		end if;
+		counter := counter + 1;
+	end loop;
+end$$;
+
+select * from contrato_1a;
+select * from contrato_1b;
+
+with datos_contrato_dulce as (
+	select
+	random() * 1000 as precio_dulces,
+	random() * ((select count(dulce_id) from dulce) - 1) as dulce_id,
+	contrato_id
+	from GENERATE_SERIES(0, 10000) seq
+	inner join contrato_1b ON (seq = contrato_id)
+)
+insert into contrato_1b_dulce(precio_dulces, dulce_id, contrato_id) select * from datos_contrato_dulce;
+select * from contrato_1b_dulce;
+
+
+with datos_maquina as (
+	select
+	(
+		case (random() * 4)::int
+			when 0 then 'Vibranium'
+			when 1 then 'Acero'
+			when 2 then 'Aluminio'
+			when 3 then 'Plástico'
+			when 4 then 'Cartón'
+		end
+	) as nombre,
+	(
+		case (random() * 2)::int
+			when 0 then 'Linux'
+			when 1 then 'Windows'
+			when 2 then 'BSD'
+		end
+	) as tipo_sistema_operativo,
+	CAST(1000000000 + floor(random() * 9000000000) as bigint) as num_registro,
+	empresa_id,
+	contrato_id
+	from GENERATE_SERIES(0, 10000) seq
+	inner join contrato ON (seq = contrato_id)
+)
+insert into maquina(material, tipo_sistema_operativo, num_registro, empresa_id, contrato_id) select * from datos_maquina;
+select * from maquina;
+
+do $$
+declare
+	counter integer := 0;
+		valor integer := 0;
+begin
+	while counter < (select count(maquina_id) from maquina) loop
+		valor := (random() * 2)::int;
+		if valor = 0 then
+			insert into tipo_A(
+				maquina_id,
+				dimensiones,
+				capacidad
+			)
+			values(
+				counter,
+				'135x200x135',
+				25
+			);
+		elsif valor = 2 then
+			insert into tipo_B(
+				maquina_id,
+				dimensiones,
+				capacidad,
+				vidrio_reforzado,
+				alerta,
+				candados
+			)
+			values(
+				counter,
+				'150x200x140',
+				35,
+				'Vidrio marca Acme',
+				'alerta.wav',
+				'los más baratos'
+			);
+		else
+			insert into tipo_C(
+				maquina_id,
+				dimensiones,
+				capacidad,
+				vidrio_templado,
+				alerta,
+				candados,
+				camara
+			)
+			values(
+				counter,
+				'135x200x135',
+				50,
+				'Vidrio marca Acme',
+				'alerta.wav',
+				'los más baratos',
+				'Go Pro'
+			);
+
+		end if;
+		counter := counter + 1;
+	end loop;
+end$$;
+
+select * from tipo_A;
+select * from tipo_B;
+select * from tipo_C;
